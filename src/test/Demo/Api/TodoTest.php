7208d5ed1dcf3366f8fd926b8cd901843e68f240
Ben Schwartz <benschw@gmail.com>
2015-12-11 14:28:23 -0600

    fixing test

---
+++ src/test/Demo/Api/TodoTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Demo\Api;
+
+class TodoTest extends \PHPUnit_Framework_TestCase {
+	public function setup() {
+	}
+	public function testApiMapper() {
+		// given
+		$todo = new Todo(12, "foo", "new");
+
+		// when
+
+		$vo = $todo->marshal();
+		$entity = Todo::unmarshal($vo);
+
+		// then
+
+		$this->assertEquals($todo, $entity, "api should be able to marshal/unmarshal without losing anything");
+	}
+}
